97798fd7354668dfa211989b93ac3651170d1416
GINN30 <adyagi14@gmail.com>
2023-03-31 22:32:57 +0700

    code tugas 2 looping & comment

---
+++ Tugas-looping/loop2.js
@@ -0,0 +1,10 @@
+//nilai awal
+let startValue = 0;
+
+//perulangan looping
+for (let i = 1; i <= 10; i++) {
+    //set nilai awal selalu di tambah 2 setiap perulangan
+    startValue += 2;
+    //menampilkan hasil
+    console.log(`Hasil penambahan pada pengulangan ke-${i} adalah ${startValue}`);
+}
